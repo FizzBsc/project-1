7be65e8ec7b52514f6cb7e3dfadc90df0ab24cf4
FizzBsc <bsc.hhafiz@gmail.com>
2022-08-30 13:21:50 +1000

    netlify-deploy3

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ import '../index.css';
 export default function Navbar(){
     return(
         <nav className="head-container">
-            <img src="logo192.png" height='40px'></img>
+            <img src="logo192.png" alt="description of image" height='40px'></img>
             <h1 className="head-start">ReactFacts</h1>
             <h2 className="head-end">React Course - Project 1</h2>
         </nav>
